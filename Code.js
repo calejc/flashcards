08f67e1ddf187edbd20cd1eba6f7d5fafd6193da
Cale Cortney <thortney@mailbox.org>
2020-07-20 17:15:39 -0500

    Update

---
+++ Code.js
@@ -0,0 +1,12 @@
+function doGet(e) {
+  var template = HtmlService.createTemplateFromFile('Index');
+  return template.evaluate().addMetaTag('viewport', 'width=device-width, initial-scale=1, shrink-to-fit=no').setSandboxMode(HtmlService.SandboxMode.IFRAME).setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+  // return template.evaluate().addMetaTag('viewport', 'width=device-width, initial-scale=1, shrink-to-fit=no').setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+
+
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename)
+      .getContent();
+  console.log('Got here!');
+}
